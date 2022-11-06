154d82707414588e53e8ca301aadcc0f3ea341d5
Gidra <ivan.savovic@yahoo.com>
2022-11-06 17:54:43 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('HELLO')
+import pip
+import math
+
+polje = bool('Polje')
+# polje_1 = 3.52
+# polje_2 = 5
+# polje_3 = 2
+polje_4 = polje_1 + polje_2 + polje_3
+print(polje_4)
+# print(polje)
