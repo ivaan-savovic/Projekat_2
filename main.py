5a01dfcab91bd668abeadc397156b2255a76782b
Gidra <ivan.savovic@yahoo.com>
2022-11-06 22:06:59 +0100

    test

--- main.py
+++ main.py
@@ -28,3 +28,11 @@ def gornje_polje():
         return True
 print(donje_polje())
 print(gornje_polje())
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+tri_recursion(8)
