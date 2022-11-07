ad22c7d5e05496330cfb2b82a66c7d1711f71f99
Gidra <ivan.savovic@yahoo.com>
2022-11-07 07:18:12 +0100

    test

--- main.py
+++ main.py
@@ -71,3 +71,23 @@ class Person:
     return f"{self.name}({self.age})"
 p1 = Person("Ivan", 37)
 print(p1)
+
+#INHERITED FUNCTIONS
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year): #parent class
+    super().__init__(fname, lname) #child class
+    self.graduationyear = year
+    x = Student("Mike", "Olsen", 2019)
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Person("John", "Doe")
+x.printname()
